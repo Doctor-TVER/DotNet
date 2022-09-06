bd37ee9ef3beaf0291f090db63e66bd405140fa1
Doctor-TVER <ilyinya@yandex.ru>
2022-09-06 06:41:42 +0300

    add solution for task 1-8

---
+++ Task4/Program.cs
@@ -0,0 +1,35 @@
+﻿// ==================================================================================
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// ==================================================================================
+
+string? inputLineA = Console.ReadLine();
+string? inputLineB = Console.ReadLine();
+string? inputLineC = Console.ReadLine();
+if (inputLineA != null && inputLineB != null && inputLineC != null)
+{
+    int inputNumberA = int.Parse(inputLineA);
+    int inputNumberB = int.Parse(inputLineB);
+    int inputNumberC = int.Parse(inputLineB);
+    if (inputNumberA > inputNumberB)
+    {
+        if(inputNumberA > inputNumberC)
+        {
+            Console.WriteLine("Max число А");
+        }
+        else
+        {
+            Console.WriteLine("Max число C");
+        }
+    }
+    else
+    {
+        if(inputNumberB > inputNumberC)
+        {
+            Console.WriteLine("Max число B");
+        }
+        else
+        {
+            Console.WriteLine("Max число C");
+        }
+    }
+}
---
+++ Task7/Program.cs
@@ -0,0 +1,11 @@
+﻿string? inputLineN = Console.ReadLine();
+
+if (inputLineN != null)
+{
+    int inputNumberN = int.Parse(inputLineN);
+    if (inputNumberN > 99 && inputNumberN < 1000)
+    {
+        int lastDigit = inputNumberN % 10;
+        Console.Write(lastDigit);
+    }
+}
